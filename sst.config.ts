0a67e3c40dcd607511b2dd5dc97def59bcee5dab
NutellaPlox <harhsieh@gmail.com>
2023-04-26 17:45:32 -0500

    initial commit

---
+++ sst.config.ts
@@ -0,0 +1,25 @@
+import { SSTConfig } from "sst"
+import { Bucket, NextjsSite } from "sst/constructs"
+
+export default {
+  config(_input) {
+    return {
+      name: "sst",
+      region: "us-east-1",
+    }
+  },
+  stacks(app) {
+    app.stack(function Site({ stack }) {
+      const bucket = new Bucket(stack, "public", {
+        cors: true,
+      })
+      const site = new NextjsSite(stack, "site", {
+        bind: [bucket],
+      })
+
+      stack.addOutputs({
+        SiteUrl: site.url,
+      })
+    })
+  },
+} satisfies SSTConfig
